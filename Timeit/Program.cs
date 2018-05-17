62a7e49e5d13e1d42c599e667850e56b77008a81
Nuno Caneco <nuno.caneco@farfetch.com>
2018-05-17 10:07:16 +0100

    Add project files.

---
+++ Timeit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Timeit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
