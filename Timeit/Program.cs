4e3fb6724dcd2494b9cecbf760d825c59c031931
Nuno Caneco <nuno.caneco@farfetch.com>
2018-05-17 11:17:26 +0100

    Launch the process and time its execution

--- Timeit/Program.cs
+++ Timeit/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Linq;
+using System.Diagnostics;
 
 namespace Timeit
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if(args.Length< 1)
+            {
+                PrintHelp();
+                return 1;
+            }
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var process = Process.Start(
+                fileName: args[0],
+                arguments: args.Length > 1 ? string.Join(" ", args.Skip(1)) : string.Empty);
+
+            process.WaitForExit();
+
+            stopwatch.Stop();
+
+            Console.WriteLine();
+            Console.WriteLine($"Command execution time: {stopwatch.Elapsed}");
+
+            return process.ExitCode;
+        }
+
+        private static void PrintHelp()
+        {
+            Console.WriteLine("Usage: timeit.exe <command> <args>");
         }
     }
 }
